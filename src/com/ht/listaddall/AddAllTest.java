a076f5cce4cc023f2c08ca28e404d06c516ceb04
安诺爱成长 <1399487511@qq.com>
2016-05-05 22:34:23 +0800

    测试深拷贝、、浅拷贝

--- src/com/ht/listaddall/AddAllTest.java
+++ src/com/ht/listaddall/AddAllTest.java
@@ -7,14 +7,12 @@ import java.util.ArrayList;
  */
 public class AddAllTest {
     public static void main(String[] args) {
-        ArrayList l1 = new ArrayList();
-        ArrayList l2 = new ArrayList();
+        ArrayList<DemoEntity> l1 = new ArrayList();
+        ArrayList<DemoEntity> l2 = new ArrayList();
 
 //        l1.addAll(l2);
         System.out.println(l1.size());
 
-        DemoEntity demoEntity = new DemoEntity();
-        l2.add(demoEntity);
         l1.addAll(l2);
 //      System.out.println(l1.size());
 //      System.out.println(l2.size());
@@ -24,13 +22,14 @@ public class AddAllTest {
 
 //      l1.removeAll(l2);
 //      System.out.println(l1.size());
+        DemoEntity demoEntity = new DemoEntity();
+        demoEntity.setState("1");
+        l2.add(demoEntity);
 
+        for (int i = 0; i < l1.size(); i++) {
+            System.out.println(l1.get(i).getState());
+        }
 
-        l1.removeAll(l2);
-
-        l2.remove(demoEntity);
-
-        System.out.println(l1.size());
 
 
     }
--- src/com/ht/listaddall/DemoEntity.java
+++ src/com/ht/listaddall/DemoEntity.java
@@ -4,13 +4,16 @@ package com.ht.listaddall;
  * Created by annuoaichengzhang on 16/5/4.
  */
 public class DemoEntity {
-    private int state;
 
-    public int getState() {
+
+
+    private String state;
+
+    public String getState() {
         return state;
     }
 
-    public void setState(int state) {
+    public void setState(String state) {
         this.state = state;
     }
 }
