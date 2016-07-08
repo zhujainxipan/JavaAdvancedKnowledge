8481279978106f6f194c8e05ef8ca66d68a25b3f
安诺爱成长 <1399487511@qq.com>
2016-07-08 16:34:48 +0800

    测试volatile和synchronized关键字的区别

---
+++ src/com/ht/concurrency/chapter2/AccountingVol.java
@@ -0,0 +1,34 @@
+package com.ht.concurrency.chapter2;
+
+/**
+ * Created by niehongtao on 16/7/8.
+ * 2.7
+ */
+public class AccountingVol implements Runnable {
+    static AccountingVol instance = new AccountingVol();
+    //    static volatile int i = 0;
+    static int i = 0;
+
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 10000000; j++) {
+            increase();
+        }
+
+    }
+
+    private synchronized void increase() {
+        i++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
+}
