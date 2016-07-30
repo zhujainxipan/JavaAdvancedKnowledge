aea3f4f37fe152b5522b9dc5957edd8b94b7a9cf
安诺爱成长 <1399487511@qq.com>
2016-07-30 22:01:29 +0800

    装箱拆箱测试

---
+++ src/com/ht/chaixiangzhuangxiang/ChaiXiangTest.java
@@ -0,0 +1,16 @@
+package com.ht.chaixiangzhuangxiang;
+
+/**
+ * Created by niehongtao on 16/7/30.
+ */
+public class ChaiXiangTest {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 1;
+        Integer c = 200;
+        Integer d = 200;
+
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+}
