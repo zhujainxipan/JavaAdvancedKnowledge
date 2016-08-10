ce6ef353c14f2f19200487f5b3e191afc412b4b7
安诺爱成长 <1399487511@qq.com>
2016-08-10 14:06:50 +0800

    重构测试

---
+++ src/com/ht/refactor/demo3/Test.java
@@ -0,0 +1,13 @@
+package com.ht.refactor.demo3;
+
+/**
+ * Created by niehongtao on 16/8/10.
+ */
+public class Test {
+    private final String day;
+
+    public Test(String day) {
+        this.day = day;
+    }
+
+}
